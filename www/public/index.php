6d4ed497a2f856b9087aa169f5408889a3168cb2
mtaopp <82059158+mtaopp@users.noreply.github.com>
2021-06-18 00:48:25 +0200

    17.6 header responsiv / head->header.inc.php / footer->footer.inc.php

--- www/public/index.php
+++ www/public/index.php
@@ -1,36 +1,4 @@
-<!DOCTYPE html>
-<html lang="en">
-
-<head>
-  <meta charset="UTF-8">
-  <meta http-equiv="X-UA-Compatible" content="IE=edge">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>PHP Include Exercise</title>
-  <link rel="stylesheet" href="/build/css/style.css">
-  
-  </head>
-  
-<body>
-
-  <header class="welcome">
-    <div class="container">
-      <div class="row">
-      <div class="logo-title">
-        <a class="logo" href="index.php"><img src="/images/logo-tq.svg"></a>
-        <h1>techQuest</h1>
-      </div>
-
-      <div class="tagline">
-        <p>Welcome to Your Quest for Knowledge!</p>
-      </div>
-
-      <div class="makeit">
-        <a href="create.php" class="btn-primary">Create Your Own Quiz!</a></li>
-                </div>  
-    </div>
-  </header>
-
-  <main class="quizzes">
+<?php require_once 'includes/header.inc.php'; ?>
 
     <div class="card" style="width: 18rem;">
       <img src="/images/tiger_filler.jpg" class="card-img-top" alt="...">
@@ -77,15 +45,4 @@
       </div>
     </div>
 
-
-  </main>
-
-  <?php
-
-  //phpinfo();
-
-  ?>
-
-</body>
-
-</html>
+  <?php require_once 'includes/footer.inc.php'; ?>
