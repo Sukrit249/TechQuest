dc40c15da976bf54dce51a3eab1195ac0bb17f16
mtaopp <m.ta.opportunity@gmail.com>
2021-06-18 18:03:44 +0200

    clean index page

--- www/public/index.php
+++ www/public/index.php
@@ -3,54 +3,6 @@
   require_once 'php/functions.php'; 
 
   addCard("regular");
+  
+  require_once 'includes/footer.inc.php'; 
 ?>
-
-
-
-<!-- 
-    <div class="card" style="width: 18rem;">
-      <img src="/images/tiger_filler.jpg" class="card-img-top" alt="...">
-      <div class="card-body">
-        <h5 class="card-title">HTML Quiz</h5>
-        <p class="card-text">Created by Name</p>
-        <a href="#" class="btn btn-primary">Play Game</a>
-      </div>
-    </div>
-
-    <div class="card" style="width: 18rem;">
-      <img src="/images/tiger_filler.jpg" class="card-img-top" alt="...">
-      <div class="card-body">
-        <h5 class="card-title">CSS Quiz</h5>
-        <p class="card-text">Created by Name</p>
-        <a href="#" class="btn btn-primary">Play Game</a>
-      </div>
-    </div>
-
-    <div class="card" style="width: 18rem;">
-      <img src="/images/tiger_filler.jpg" class="card-img-top" alt="...">
-      <div class="card-body">
-        <h5 class="card-title">JavaScript Quiz</h5>
-        <p class="card-text">Created by Name</p>
-        <a href="#" class="btn btn-primary">Play Game</a>
-      </div>
-    </div>
-
-    <div class="card" style="width: 18rem;">
-      <img src="/images/tiger_filler.jpg" class="card-img-top" alt="...">
-      <div class="card-body">
-        <h5 class="card-title">Git Quiz</h5>
-        <p class="card-text">Created by Name</p>
-        <a href="#" class="btn btn-primary">Play Game</a>
-      </div>
-    </div>
-
-    <div class="card" style="width: 18rem;">
-      <img src="/images/tiger_filler.jpg" class="card-img-top" alt="...">
-      <div class="card-body">
-        <h5 class="card-title">Sass Quiz</h5>
-        <p class="card-text">Created by Name</p>
-        <a href="#" class="btn btn-primary">Play Game</a>
-      </div>
-    </div>
- -->
-  <?php require_once 'includes/footer.inc.php'; ?>
