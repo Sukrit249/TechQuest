c8cdea23524e5f9b33eb0a9dc6f085eaf2f73e4b
mtaopp <m.ta.opportunity@gmail.com>
2021-06-24 15:19:16 +0200

    last commit

--- www/public/test/quiz.php
+++ www/public/test/quiz.php
@@ -1,16 +1,7 @@
 <?php
   session_start(); // starting/continue the session
-  // Creating a session array with a question and the correct answer
-  $_SESSION['questions'] = [
-    ["Question ONE?", "Correct Answer for question one"],
-    ["Question TWO?", "Correct Answer for question two"]
-  ];
-  // Creating a session array with answer 
-  // !! One answer has to match the correct answer that is defined in the $_SESSION['questions']
-  $_SESSION['answers'] = [
-    ["Wrong Answer for question one", "Wrong Answer for question one", "Correct Answer for question one"],
-    ["Wrong Answer for question two", "Correct Answer for question two", "Wrong Answer for question two"]
-  ];
+
+  include '../quiz/regular/php/create.php';
 
   // Creating an empty array for the answers given by the client
   $_SESSION['inputs'] = [];
@@ -18,6 +9,7 @@
   if (!isset($_SESSION['quiz_counter'])) { // If the $_SEEION['quiz_counter'] is not already set 
     // Creating a session variable with a counter for the question selection
     $_SESSION['quiz_counter'] = 0;
+    $_SESSION['score'] = 0;
   }
   // just a shorter way to get the quiz counter value
   $counter = $_SESSION['quiz_counter'];
@@ -29,11 +21,11 @@
 
   <h1><?php $_SESSION['questions'][$counter][0]; ?></h1>
 
-  <input type="radio" name="A" id="A1" value="<?php echo $_SESSION['answers'][$counter][0]; ?>">
-  <label for="A1"><?php echo $_SESSION['answers'][$counter][0]; ?></label><br>
-  <input type="radio" name="A" id="A2" value="<?php echo $_SESSION['answers'][$counter][1]; ?>">
-  <label for="A2"><?php echo $_SESSION['answers'][$counter][1]; ?></label><br>
-  <input type="radio" name="A" id="A3" Value="<?php echo $_SESSION['answers'][$counter][1]; ?>">
-  <label for="A3"><?php echo $_SESSION['answers'][$counter][2]; ?></label><br>
+  <input type="radio" name="A" id="A1" value="<?php echo $as[$counter][0]; ?>">
+  <label for="A1"><?php echo $as][$counter][0]; ?></label><br>
+  <input type="radio" name="A" id="A2" value="<?php echo $as[$counter][1]; ?>">
+  <label for="A2"><?php echo $as[$counter][1]; ?></label><br>
+  <input type="radio" name="A" id="A3" Value="<?php echo $as[$counter][1]; ?>">
+  <label for="A3"><?php echo $as[$counter][2]; ?></label><br>
   <button>SENDEN</button>
 </form>
--- www/public/test/result.php
+++ www/public/test/result.php
@@ -1,13 +1,14 @@
 <?php
   session_start();  //starting/continue the session
+  include '../quiz/regular/php/create.php';
 
   if(!isset($_POST['A'])) {                                                 // if no value in $_POST['A'] is submited
       header('Location: quiz.php');                                         // load the quiz page 
   } else {                                                                  
     array_push($_SESSION['inputs'], $_POST['A']);                           // add the input given by the client to the $_SESSION['inputs'] array
     $_SESSION['quiz_counter']++;                                            // add 1 to the question counter
-    if ($_SESSION['quiz_counter'] == count($_SESSION['questions'])) {       // if the question counter is equal to the number of elements in the $_SESSION['questions']
-      echo "HERE IS YOUR RESULT"; // add validation here and delete echo =)
+    if ($_SESSION['quiz_counter'] == count($qs)) {                          // if the question counter is equal to the number of elements in the $_SESSION['questions']
+      echo "HERE IS YOUR RESULT";             // add validation here and delete echo =)
       session_destroy();                                                     // End the session 
     } else {
       header('Location: quiz.php');                                          // load the quiz page
