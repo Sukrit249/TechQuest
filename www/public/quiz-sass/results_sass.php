0d4f06b667e2f5a8a49646136461daaae5a2419b
atournikate <kate.ebensteiner@gmail.com>
2021-06-24 11:02:16 +0200

    do 24.06 1102 kme 85%

--- www/public/quiz-sass/results_sass.php
+++ www/public/quiz-sass/results_sass.php
@@ -4,9 +4,14 @@
   require_once '../quiz/regular/sass/create.php';
 
   $_SESSION['quiz_counter']++;
-  
+
   if ($_SESSION['quiz_counter'] == count($qa)) {
-    echo "You got " . $_SESSION['score'] . " out of" . count($qa) . " correct!";
+    $score = $_SESSION['score'];
+    require_once '../php/includes/header.inc.php';       // Includes the header
+  require_once '../php/functions.php';             // Includes the functions.php
+    echo "<div class='congrats'> <h3>You got " . $score . " out of" . count($qa) . " correct!</h3>";
+    echo '<button type="reset" class="btn-primary">Try Again</button>';
+    session_destroy();
   } else {
       header("Location: quiz_sass.php");
   }
